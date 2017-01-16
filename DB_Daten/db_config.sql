982793b805adfbf43fb06740cb1911a56d5a7d03
nwelping <niklas.welpinghaus@hs-osnabrueck.de>
2017-01-16 15:05:08 +0100

    Datenbank-Scripte

---
+++ DB_Daten/db_config.sql
@@ -0,0 +1,174 @@
+CREATE TABLE Sektor (
+	id_sektor BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	stoerung INT,
+	position_x INT,
+	position_y INT,
+	position_z INT,
+	position_ausrichtung INT,
+	PRIMARY KEY (id_sektor)
+	);
+
+CREATE TABLE Warentraeger (
+	id_warentraeger BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	stoerung INT,
+	montagezustand INT,
+	RFID_inhalt CHAR(128),
+	abstand_mm INT,
+	PRIMARY KEY (id_warentraeger)
+	);
+
+CREATE TABLE Artikel (
+	id_artikel BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	PRIMARY KEY (id_artikel)
+	);
+	
+CREATE TABLE Transportband (
+	id_transportband BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	reihe INT,
+	stoerung INT,
+	laenge INT,
+	geschwindigkeit INT,
+	id_sektor_vor BIGINT,
+	id_sektor_nach BIGINT,
+	PRIMARY KEY (id_transportband),
+	FOREIGN KEY (id_sektor_vor) REFERENCES Sektor(id_sektor),
+	FOREIGN KEY (id_sektor_nach) REFERENCES Sektor(id_sektor)
+	);
+	
+CREATE TABLE Sensor (
+	id_sensor BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	stoerung INT,
+	zustand INT,
+	phy_adresse CHAR(10),
+	id_sektor BIGINT,
+	PRIMARY KEY (id_sensor),
+	FOREIGN KEY (id_sektor) REFERENCES Sektor(id_sektor)
+	);
+
+CREATE TABLE Roboter (
+	id_roboter BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	stoerung INT,
+	position_x INT,
+	position_y INT,
+	position_z INT,
+	position_ausrichtung INT,
+	PRIMARY KEY (id_roboter)
+	);
+	
+CREATE TABLE Gelenk (
+	id_gelenk BIGINT,	
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	typ VARCHAR(100),
+	nummer INT,
+	gelenkstellung INT,
+	id_roboter BIGINT,
+	PRIMARY KEY (id_gelenk),
+	FOREIGN KEY (id_roboter) REFERENCES Roboter(id_roboter)
+	);
+	
+CREATE TABLE Werkzeug (
+	id_werkzeug BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	zustand INT,
+	id_roboter BIGINT,
+	PRIMARY KEY (id_werkzeug),
+	FOREIGN KEY (id_roboter) REFERENCES Roboter(id_roboter)
+	);
+	
+CREATE TABLE Hubpodest (
+	id_hubpodest BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	oben INT,
+	unten INT,
+	id_sektor BIGINT,
+	PRIMARY KEY (id_hubpodest),
+	FOREIGN KEY (id_sektor) REFERENCES Sektor(id_sektor)
+	);
+	
+CREATE TABLE Hubquerpodest (
+	id_hubquerpodest BIGINT,
+	bezeichnung VARCHAR(100),
+	zeitstempel TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	user_parameter LONGTEXT,
+	motor INT,
+	oben INT,
+	mittig INT,
+	unten INT,
+	id_sektor BIGINT,
+	PRIMARY KEY (id_hubquerpodest),
+	FOREIGN KEY (id_sektor) REFERENCES Sektor(id_sektor)
+	);
+	
+CREATE TABLE Artikel_Warentraeger (
+	id BIGINT AUTO_INCREMENT,
+	id_artikel BIGINT,
+	id_warentraeger BIGINT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_artikel) REFERENCES Artikel(id_artikel),
+	FOREIGN KEY (id_warentraeger) REFERENCES Warentraeger(id_warentraeger)
+	);
+	
+CREATE TABLE Sektor_Warentraeger (
+	id_sektor BIGINT,
+	id_warentraeger BIGINT,
+	PRIMARY KEY (id_sektor, id_warentraeger),
+	FOREIGN KEY (id_sektor) REFERENCES Sektor(id_sektor),
+	FOREIGN KEY (id_warentraeger) REFERENCES Warentraeger(id_warentraeger)
+	);
+	
+CREATE TABLE Transportband_Warentraeger (
+	id_transportband BIGINT,
+	id_warentraeger BIGINT,
+	PRIMARY KEY (id_transportband, id_warentraeger),
+	FOREIGN KEY (id_transportband) REFERENCES Transportband(id_transportband),
+	FOREIGN KEY (id_warentraeger) REFERENCES Warentraeger(id_warentraeger)
+	);
+	
+CREATE TABLE Roboter_Sektor (
+	id_roboter BIGINT,
+	id_sektor BIGINT,
+	PRIMARY KEY (id_roboter, id_sektor),
+	FOREIGN KEY (id_roboter) REFERENCES Roboter(id_roboter),
+	FOREIGN KEY (id_sektor) REFERENCES Sektor(id_sektor)
+	);
+	
+CREATE TABLE Roboter_Werkzeug (
+	id_roboter BIGINT,
+	id_werkzeug BIGINT,
+	PRIMARY KEY (id_roboter, id_werkzeug),
+	FOREIGN KEY (id_roboter) REFERENCES Roboter(id_roboter),
+	FOREIGN KEY (id_werkzeug) REFERENCES Werkzeug(id_werkzeug)
+	);
+	
+CREATE TABLE Hubquerpodest_Hubquerpodest (
+	id_Hubquerpodest1 BIGINT,
+	id_hubquerpodest2 BIGINT,
+	PRIMARY KEY (id_hubquerpodest1, id_hubquerpodest2),
+	FOREIGN KEY (id_hubquerpodest1) REFERENCES Hubquerpodest(id_hubquerpodest),
+	FOREIGN KEY (id_hubquerpodest2) REFERENCES Hubquerpodest(id_hubquerpodest)
+	);
+	
---
+++ DB_Daten/db_insert4_Transportband.sql
@@ -0,0 +1,375 @@
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		13,
+		'TB Sek1 bis Sek3',
+		'',
+		0,
+		0,
+		0,
+		0,
+		1,
+		3
+	);
+
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		34,
+		'TB Sek3 bis Sek4',
+		'',
+		0,
+		0,
+		0,
+		0,
+		3,
+		4
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		45,
+		'TB Sek4 bis Sek5',
+		'',
+		0,
+		0,
+		0,
+		0,
+		4,
+		5
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		56,
+		'TB Sek5 bis Sek6',
+		'',
+		0,
+		0,
+		0,
+		0,
+		5,
+		6
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		67,
+		'TB Sek6 bis Sek7',
+		'',
+		0,
+		0,
+		0,
+		0,
+		6,
+		7
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		78,
+		'TB Sek7 bis Sek8',
+		'',
+		0,
+		0,
+		0,
+		0,
+		7,
+		8
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		89,
+		'TB Sek8 bis Sek9',
+		'',
+		0,
+		0,
+		0,
+		0,
+		8,
+		9
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		910,
+		'TB Sek9 bis Sek10 (aussen)',
+		'',
+		1,
+		0,
+		0,
+		0,
+		9,
+		10
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		1011,
+		'TB Sek10 bis Sek11 (aussen)',
+		'',
+		0,
+		0,
+		0,
+		0,
+		10,
+		11
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		911,
+		'TB Sek9 bis Sek11 (innen)',
+		'',
+		-1,
+		0,
+		0,
+		0,
+		9,
+		11
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		1112,
+		'TB Sek11 bis Sek12 (aussen)',
+		'',
+		1,
+		0,
+		0,
+		0,
+		11,
+		12
+	);
+
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		1213,
+		'TB Sek12 bis Sek13 (aussen)',
+		'',
+		0,
+		0,
+		0,
+		0,
+		12,
+		13
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		1113,
+		'TB Sek11 bis Sek13 (innen)',
+		'',
+		-1,
+		0,
+		0,
+		0,
+		11,
+		13
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		1314,
+		'TB Sek13 bis Sek14 (aussen)',
+		'',
+		1,
+		0,
+		0,
+		0,
+		13,
+		14
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		1415,
+		'TB Sek14 bis Sek15 (aussen)',
+		'',
+		0,
+		0,
+		0,
+		0,
+		14,
+		15
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		1315,
+		'TB Sek13 bis Sek15 (innen)',
+		'',
+		-1,
+		0,
+		0,
+		0,
+		13,
+		15
+	);
+
+INSERT INTO Transportband (
+		id_transportband,
+		bezeichnung,
+		user_parameter,
+		reihe,
+		stoerung,
+		laenge,
+		geschwindigkeit,
+		id_sektor_vor,
+		id_sektor_nach
+	) VALUES (
+		151,
+		'TB Sek15 bis Sek1',
+		'',
+		0,
+		0,
+		0,
+		0,
+		15,
+		1
+	);
