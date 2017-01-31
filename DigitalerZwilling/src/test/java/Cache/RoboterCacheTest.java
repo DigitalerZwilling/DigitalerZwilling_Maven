a37acf5c09729c9f2d1f6d84cd3debe5887a7472
Florian Kruckmann <florian.kruckmann@hs-osnabrueck.de>
2017-01-31 16:18:23 +0100

    zwischen Ergebnis

--- DigitalerZwilling/src/test/java/Cache/RoboterCacheTest.java
+++ DigitalerZwilling/src/test/java/Cache/RoboterCacheTest.java
@@ -5,6 +5,7 @@
  */
 package Cache;
 
+import DatenbankTestInsert.DatenbankTestInsert;
 import de.hsos.digitalerzwilling.Cache.Cache;
 import de.hsos.digitalerzwilling.Cache.RoboterCache;
 import de.hsos.digitalerzwilling.Cache.Updater.CacheUpdateThread;
@@ -12,6 +13,8 @@ import de.hsos.digitalerzwilling.Cache.Updater.SelfTimer;
 import de.hsos.digitalerzwilling.Cache.Updater.Updater;
 import de.hsos.digitalerzwilling.Cache.Updater.WebSocketUpdateThread;
 import de.hsos.digitalerzwilling.DatenbankSchnittstelle.DatenbankSchnittstelle;
+import de.hsos.digitalerzwilling.DatenbankSchnittstelle.Exception.DBNotFoundException;
+import de.hsos.digitalerzwilling.DatenbankSchnittstelle.Exception.QueryException;
 import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -42,11 +45,35 @@ public class RoboterCacheTest extends CacheTest{
     RoboterCache cache;
 
     @Before
-    public void setUp() {
+    public void setUp() throws DBNotFoundException, QueryException {
+        tearDown();
+        datenbankSchnittstelle.connect("./testdbConfig.cfg");
+        DatenbankTestInsert datenbankTestInsert = new DatenbankTestInsert();
+        datenbankTestInsert.datenbankUpdate("INSERT INTO ROBOTER (ID_ROBOTER,BEZEICHNUNG,STOERUNG,POSITION_X,POSITION_Y,POSITION_Z,POSITION_AUSRICHTUNG) VALUES (4242,'CacheTestRoboter1',0,0,0,0,0)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO SEKTOR (ID_SEKTOR,BEZEICHNUNG,STOERUNG,POSITION_X,POSITION_Y,POSITION_Z,POSITION_AUSRICHTUNG) VALUES (4242,'CacheTestSektor1',0,10,10,10,45)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO SEKTOR (ID_SEKTOR,BEZEICHNUNG,STOERUNG,POSITION_X,POSITION_Y,POSITION_Z,POSITION_AUSRICHTUNG) VALUES (4243,'CacheTestSektor2',0,10,20,10,45)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO ROBOTER_SEKTOR (ID_SEKTOR,ID_ROBOTER VALUES (4242,4242)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO GELENK (ID_GELENK,BEZEICHNUNG,TYP,NUMMER,GELENKSTELLUNG,ID_ROBOTER) VALUES (4242,'CacheTestGELENK1','Typ1',2,0,4242)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO GELENK (ID_GELENK,BEZEICHNUNG,TYP,NUMMER,GELENKSTELLUNG,ID_ROBOTER) VALUES (4243,'CacheTestGELENK2','Typ2',3,90,4242)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO GELENK (ID_GELENK,BEZEICHNUNG,TYP,NUMMER,GELENKSTELLUNG,ID_ROBOTER) VALUES (4244,'CacheTestGELENK3','Typ3',4,180,4242)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO WERKZEUG (ID_WERKZEUG,BEZEICHNUNG,ZUSTAND) VALUES (4242,'CacheTestWerkzeug1',1)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO ROBOTER_WERKZEUG (ID_ROBOTER,ID_WERKZEUG) VALUES (4242,4242)");
+        datenbankTestInsert.close();
     }
     
     @After
-    public void tearDown() {
+    public void tearDown() throws DBNotFoundException, QueryException {
+        DatenbankTestInsert datenbankTestInsert = new DatenbankTestInsert();
+        datenbankTestInsert.datenbankUpdate("DELETE FROM ROBOTER WHERE ID_ROBOTER = 4242");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO SEKTOR (ID_SEKTOR,BEZEICHNUNG,STOERUNG,POSITION_X,POSITION_Y,POSITION_Z,POSITION_AUSRICHTUNG) VALUES (4242,'CacheTestSektor1',0,10,10,10,45)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO SEKTOR (ID_SEKTOR,BEZEICHNUNG,STOERUNG,POSITION_X,POSITION_Y,POSITION_Z,POSITION_AUSRICHTUNG) VALUES (4243,'CacheTestSektor2',0,10,20,10,45)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO ROBOTER_SEKTOR (ID_SEKTOR,ID_ROBOTER VALUES (4242,4242)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO GELENK (ID_GELENK,BEZEICHNUNG,TYP,NUMMER,GELENKSTELLUNG,ID_ROBOTER) VALUES (4242,'CacheTestGELENK1','Typ1',2,0,4242)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO GELENK (ID_GELENK,BEZEICHNUNG,TYP,NUMMER,GELENKSTELLUNG,ID_ROBOTER) VALUES (4243,'CacheTestGELENK2','Typ2',3,90,4242)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO GELENK (ID_GELENK,BEZEICHNUNG,TYP,NUMMER,GELENKSTELLUNG,ID_ROBOTER) VALUES (4244,'CacheTestGELENK3','Typ3',4,180,4242)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO WERKZEUG (ID_WERKZEUG,BEZEICHNUNG,ZUSTAND) VALUES (4242,'CacheTestWerkzeug1',1)");
+        datenbankTestInsert.datenbankUpdate("INSERT INTO ROBOTER_WERKZEUG (ID_ROBOTER,ID_WERKZEUG) VALUES (4242,4242)");
+        datenbankTestInsert.close();
     }
 
     @Override
