779b28e6193658f358b28d48f024f3e9bf6ae497
nwelping <niklas.welpinghaus@hs-osnabrueck.de>
2017-02-07 16:23:57 +0100

    DB koppeltabellen

---
+++ DB_Daten/db_insert13_Hubquerpodest_Hubquerpodest.sql
@@ -0,0 +1,33 @@
+INSERT INTO Hubquerpodest_Hubquerpodest (
+	id_Hubquerpodest1,
+	id_hubquerpodest2
+	) VALUES (
+	111,
+	112
+	);
+	
+INSERT INTO Hubquerpodest_Hubquerpodest (
+	id_Hubquerpodest1,
+	id_hubquerpodest2
+	) VALUES (
+	113,
+	114
+	);
+	
+INSERT INTO Hubquerpodest_Hubquerpodest (
+	id_Hubquerpodest1,
+	id_hubquerpodest2
+	) VALUES (
+	131,
+	132
+	);
+	
+INSERT INTO Hubquerpodest_Hubquerpodest (
+	id_Hubquerpodest1,
+	id_hubquerpodest2
+	) VALUES (
+	133,
+	134
+	);
+	
+COMMIT;
